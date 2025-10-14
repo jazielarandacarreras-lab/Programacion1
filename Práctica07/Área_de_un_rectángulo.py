974fd6410315eb6b4669c5f4ea389fcd814b8f00
Jaziel Aranda~ <jazielarandacarreras@gmail.com>
2025-10-14 08:22:51 -0700

    Quinto commit

---
+++ Práctica07/Área_de_un_rectángulo.py
@@ -0,0 +1,8 @@
+print("°-Área de un rectángulo-°")
+base = 10
+altura = 5
+def calcular_area_rectangulo(altura, base):
+    return altura * base
+area = calcular_area_rectangulo(altura, base)
+
+print(f"El área del rectángulo es: {area}")
