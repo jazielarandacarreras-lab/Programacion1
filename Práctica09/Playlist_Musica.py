8af61bfcd9d51cc465ba9d6eea41bce0fcb4ed83
Jaziel Aranda~ <jazielarandacarreras@gmail.com>
2025-10-30 14:52:40 -0700

    Doceavo commit

---
+++ Práctica09/Playlist_Musica.py
@@ -0,0 +1,7 @@
+Playlist_Musica =["Bohemian Rhapsody", "Hotel California", "Starway To Heaven"]
+Playlist_Musica.append("Happier Than Ever")
+Playlist_Musica[1] = "Shape of You"
+Playlist_Musica.insert(0,"Watermelon Sugar")
+Playlist_Musica.pop()
+print(f"{Playlist_Musica}")
+print("La cantidad de canciones es: ", len(Playlist_Musica))
